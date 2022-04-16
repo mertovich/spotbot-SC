1f59def873f2d6a17bf58dce6460c32db85a6de7
mertovich <mdsabinov@gmail.com>
2022-04-16 11:09:30 +0300

    add(API): `bot/token`

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
-const bot = require('./bot')
+const bodyParser = require('body-parser')
+const fs = require('fs');
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.post('/bot/token',urlencodedParser, function (req, res) {
+  console.log(req.body.Bot_Token)
+  // write the token to the file
+  fs.writeFile('.env', `TOKEN=${req.body.Bot_Token}`, function (err) {
+    if (err) throw err;
+    console.log('Token saved!');
+  });
+  res.send('Token saved!')
+  res.status(200)
+  res.end()
 })
 
 app.listen(3000)
