e950bbb121495d27828201e4da69800762e98c3a
mertovich <mdsabinov@gmail.com>
2022-04-17 19:32:42 +0300

    add(dataManager): get user list

--- dataManager.js
+++ dataManager.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./data.db');
+const fs = require('fs');
 
 module.exports.createDB = function(userList) {
     db.serialize(() => {
@@ -8,11 +9,26 @@ module.exports.createDB = function(userList) {
         const stmt = db.prepare("INSERT OR IGNORE INTO users(id,name,image) VALUES (?, ?, ?)");
         userList.forEach(element => {
                 stmt.run(element.id,element.name,element.image);
+                console.log(element.id + ": " + element.name + ": " + element.image);
         });
         stmt.finalize();
-
-        db.each("SELECT id, name, image FROM users", (err, row) => {
-            console.log(row.id + ": " + row.name + ": " + row.image);
-        });
     });
 }
+
+function getUsers() {
+    var userList = []
+    db.each("SELECT id, name, image FROM users", (err, row) => {
+      userList.push({
+        id: row.id,
+        name: row.name,
+        image: row.image
+      })
+      fs.writeFile('users.txt',`${JSON.stringify(userList)}`, function (err, data) {
+        if (err) throw err;
+      });
+    });
+    var data = fs.readFileSync('users.txt', 'utf-8');
+    return data
+}
+
+exports.Users = getUsers();
