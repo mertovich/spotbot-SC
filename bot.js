d787633bb8f140d4db59d9f743629452ba983721
mertovich <mdsabinov@gmail.com>
2022-04-17 16:05:54 +0300

    add(Bot): `users` Registers the users on the server to the database

--- bot.js
+++ bot.js
@@ -1,9 +1,11 @@
 const { Client, Intents, GuildMember,Guild} = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 require('dotenv').config()
+const dataManager = require('./dataManager');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`${client.guilds.cache.size} guilds`);
 });
 
 client.on('message', async message => {
@@ -18,6 +20,19 @@ client.on('message', async message => {
         const userList = list.members.cache.map(member => member.user.id);
         var item = userList[Math.floor(Math.random()*userList.length)];
         await message.reply(`🎉Winner is <${'\@'+item}>`);
+    }
+
+    if (message.content == 'users') {
+        let serverID = message.guild.id;
+        const list = client.guilds.cache.get(serverID); 
+        const userList = list.members.cache.map(member => {
+            return {
+                id: member.user.id,
+                name: member.user.username,
+                image:`https://cdn.discordapp.com/avatars/${member.user.id}/${member.user.avatar}.png?size=256`
+            }
+        });
+        dataManager.createDB(userList);
     }
 });
 
