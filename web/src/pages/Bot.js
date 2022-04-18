6a29c21b9514c4a14f31e9031e3c537cbd78056f
mertovich <mdsabinov@gmail.com>
2022-04-18 20:53:55 +0300

    add(Web): Bot register

--- web/src/pages/Bot.js
+++ web/src/pages/Bot.js
@@ -3,24 +3,43 @@ import BotTable from '../components/BotTable'
 
 export default class Bot extends Component {
   state = {
-    BotInfo: [],
+    BotInfo: []
   }
 
   getBotInfo = () => {
     const headers = { 'Content-Type': 'application/json' }
     fetch('http://localhost:3000/bot', { headers })
-        .then(response => response.json())
-        .then(data => this.setState({ BotInfo: data }));
+      .then(response => response.json())
+      .then(data => this.setState({ BotInfo: data }));
   }
+
+  registerBot = () => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ Bot_Token: this.state.botToken, Bot_Name: this.state.botName })
+    };
+    fetch('http://localhost:3000/bot/token', requestOptions)
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.registerBot()
+  }
+
 
   componentDidMount() {
     this.getBotInfo()
-}
-  
+  }
+
   render() {
     return (
       <div>
-        <BotTable BotInfo={this.state.BotInfo} />
+        <BotTable BotInfo={this.state.BotInfo} handleChange={this.handleChange} handleSubmit={this.handleSubmit}  />
       </div>
     )
   }
