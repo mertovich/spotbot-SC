6b5bd6b6fea570dfb34421978a6781c74f6c32fe
mertovich <mdsabinov@gmail.com>
2022-04-18 11:39:14 +0300

    add(Web): Router

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import {Route, Routes} from 'react-router-dom'
+import Bot from './pages/Bot'
+import Users from './pages/Users'
 
 const App = () => {
   return (
     <div>
       <NavBar/>
+    <Routes>
+      <Route path='/' element={<Bot/>} />
+      <Route path='/users' element={<Users/>} />
+    </Routes>
     </div>
   )
 }
