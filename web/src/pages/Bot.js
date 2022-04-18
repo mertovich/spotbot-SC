fd622fc6180d80f261452076b5e76d9a8595c409
mertovich <mdsabinov@gmail.com>
2022-04-18 14:07:32 +0300

    add(Web): Bot info  `/bot`

--- web/src/pages/Bot.js
+++ web/src/pages/Bot.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+import BotTable from '../components/BotTable'
 
-const Bot = () => {
-  return (
-    <div>Bot</div>
-  )
+export default class Bot extends Component {
+  state = {
+    BotInfo: [{}],
+  }
+
+  getBotInfo = () => {
+    const headers = { 'Content-Type': 'application/json' }
+    fetch('http://localhost:3000/bot', { headers })
+        .then(response => response.json())
+        .then(data => this.setState({ BotInfo: data }));
+  }
+
+  componentDidMount() {
+    this.getBotInfo()
 }
-
-export default Bot
+  
+  render() {
+    return (
+      <div>
+        <BotTable BotInfo={this.state.BotInfo} />
+      </div>
+    )
+  }
+}
